bcda58adc06e76bf4333ee0e8ef9c2cdd10e11ea
Patrick Simpson <izerop@gmail.com>
2023-10-01 21:49:22 -0400

    init

--- src/client.js
+++ src/client.js
@@ -1,52 +1,130 @@
-/* eslint-env browser */
-/* global PARTYKIT_HOST */
-
 import "./styles.css";
 
+import * as PIXI from "pixi.js";
 import PartySocket from "partysocket";
 
 let pingInterval;
 
-/** @type {HTMLDivElement} - The DOM element to append all messages we get */
 const output = document.getElementById("app");
 
-/**
- * Helper function to add a new line to the DOM
- * @param {string} text - The text to be added
- */
 function add(text) {
   output.appendChild(document.createTextNode(text));
   output.appendChild(document.createElement("br"));
 }
 
-/**
- * A PartySocket is like a WebSocket, but with more features.
- * It handles reconnection logic, buffering messages while it's offline, etc.
- * @type {PartySocket} - The connection object
- */
 const conn = new PartySocket({
   host: PARTYKIT_HOST,
   room: "my-new-room",
 });
 
-/**
- * Event listener to handle received messages.
- * @param {Event} event - The message event
- */
+let name = "anon";
+const bunny = PIXI.Sprite.from("/blob_aww.png");
+
+const players = {};
+let me = null;
+
 conn.addEventListener("message", function (event) {
-  add(`Received -> ${event.data}`);
+  let updates = JSON.parse(event.data);
+  let serverPlayers = Object.keys(updates.positions);
+  let positions = updates.positions;
+  output.innerText = `Players: ${serverPlayers.length}`;
+  serverPlayers.forEach((player) => {
+    if (players[player]?.status) {
+      let [x, y] = positions[player].split(",");
+      if (x == "dead" || y == "dead") {
+        players[player].status = false;
+        let dead = players[player].token;
+        killPlayer(dead);
+        delete players[player];
+      } else {
+        if (player != me) {
+          players[player].token.x = x;
+          players[player].token.y = y;
+        }
+      }
+    } else {
+      players[player] = {};
+      players[player].status = "alive";
+      players[player].position = updates[player];
+      if (player != me) {
+        players[player].token = PIXI.Sprite.from(
+          "https://pixijs.com/assets/bunny.png"
+        );
+        let [x, y] = positions[player].split(",");
+        players[player].token.x = x;
+        players[player].token.y = y;
+        players[player].token.anchor.set(0.5);
+        app.stage.addChild(players[player].token);
+      }
+    }
+  });
 });
 
-/**
- * Event listener for when the connection opens.
- */
 conn.addEventListener("open", function () {
-  add("Connected!");
-  add("Sending a ping every 2 seconds...");
-
-  // TODO: make this more interesting / nice
-  clearInterval(pingInterval);
-  pingInterval = setInterval(function () {
-    conn.send("ping");
-  }, 1000);
+  if (name) {
+    bunny.anchor.set(0.5);
+    bunny.x = app.screen.width / 2;
+    bunny.y = app.screen.height / 2;
+    bunny.height = 37;
+    bunny.width = 26;
+    app.stage.addChild(bunny);
+    conn.send(`spawn;${bunny.x},${bunny.y}`);
+    me = conn._pk;
+  }
 });
+
+const app = new PIXI.Application({
+  background: "#1099bb",
+  resizeTo: window,
+});
+
+document.body.appendChild(app.view);
+
+const SIZE = 16;
+
+document.addEventListener("keydown", (event) => {
+  var move = event.key;
+  var code = event.code;
+
+  if (code == "Space") {
+    conn.send(`fire;${bunny.x},${bunny.y}`);
+    return;
+  }
+
+  if (move == "w" || move == "up") {
+    bunny.y = bunny.y - SIZE;
+  }
+  if (move == "s" || move == "down") {
+    bunny.y = bunny.y + SIZE;
+  }
+  if (move == "a" || move == "left") {
+    bunny.x = bunny.x - SIZE;
+  }
+  if (move == "d" || move == "right") {
+    bunny.x = bunny.x + SIZE;
+  }
+  conn.send(`move;${bunny.x},${bunny.y}`);
+});
+
+function removePlayerToken(player) {
+  app.stage.removeChild(player);
+}
+
+function killPlayer(dead) {
+  let flush = 0;
+  const ticker = new PIXI.Ticker();
+  ticker.add((delta) => {
+    flush++;
+    deadTicker(delta, dead, flush, ticker);
+  });
+  ticker.start();
+}
+
+function deadTicker(delta, dead, flush, ticker) {
+  if (flush < 100) {
+    dead.rotation += 0.25 * delta;
+  } else {
+    removePlayerToken(dead);
+    ticker.destroy();
+  }
+}
--- src/server.js
+++ src/server.js
@@ -1,46 +1,29 @@
-/* eslint-env browser */
-
-// @ts-check
-// Optional JS type checking, powered by TypeScript.
-/** @typedef {import("partykit/server").Party} Party */
-/** @typedef {import("partykit/server").Server} Server */
-/** @typedef {import("partykit/server").Connection} Connection */
-/** @typedef {import("partykit/server").ConnectionContext} ConnectionContext */
-
-/**
- * @implements {Server}
- */
 class PartyServer {
   constructor(party) {
-    /** @type {Party} */
     this.party = party;
+    this.messages = [];
+    this.positions = {};
   }
 
-  /**
-   * @param {Connection} conn - The connection object.
-   * @param {ConnectionContext} ctx - The context object.
-   */
   onConnect(conn, ctx) {
-    // A websocket just connected!
-    console.log(
-      `Connected:
-  id: ${conn.id}
-  room: ${this.party.id}
-  url: ${new URL(ctx.request.url).pathname}`
-    );
-
-    // Send a message to the connection
-    conn.send("hello from server");
+    conn.send(JSON.stringify({ positions: this.positions }));
   }
 
-  /**
-   * @param {string} message
-   * @param {Connection} sender
-   */
+  onClose(conn) {
+    this.positions[conn.id] = "dead,dead";
+    this.party.broadcast(JSON.stringify({ positions: this.positions }));
+    delete this.positions[conn.id];
+  }
+
   onMessage(message, sender) {
-    console.log(`connection ${sender.id} sent message: ${message}`);
-    // Broadcast the received message to all other connections in the room except the sender
-    this.party.broadcast(`${sender.id}: ${message}`, [sender.id]);
+    let command = message.split(";");
+    if (command[0] == "move") {
+      this.positions[sender.id] = command[1];
+      this.party.broadcast(JSON.stringify({ positions: this.positions }));
+    }
+    if (command[0] == "fire") {
+      console.log("SHOOT!");
+    }
   }
 }
 
